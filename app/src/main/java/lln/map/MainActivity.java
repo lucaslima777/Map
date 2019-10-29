7310ec73c9bcb38d6e653355c20d04b8cade2b79
Lucas Lima T <lukaslymauchiha@gmail.com>
2019-10-28 22:15:20 -0300

    Create Map

--- app/src/main/java/lln/map/MainActivity.java
+++ app/src/main/java/lln/map/MainActivity.java
@@ -1,24 +1,52 @@
 package lln.map;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Map<Integer, String> map = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Map<String, Integer> so = new HashMap<>();
+        setInitialMap();
 
-        so.put("Android", 9);
-        so.put("iOS", 11);
-        so.put("Windows", 10);
+    }
 
+    private void setInitialMap() {
+        map.put(1, "A");
+        map.put(2, "B");
+        map.put(3, "C");
+        map.put(4, "D");
+        map.put(5, "E");
+        map.put(6, "F");
+        map.put(7, "G");
+        map.put(8, "H");
+        map.put(9, "I");
+        map.put(10, "J");
+        map.put(11, "K");
+        map.put(12, "L");
+        map.put(13, "M");
+        map.put(14, "N");
+        map.put(15, "O");
+        map.put(16, "P");
+        map.put(17, "Q");
+        map.put(18, "R");
+        map.put(19, "S");
+        map.put(20, "T");
+        map.put(21, "U");
+        map.put(22, "V");
+        map.put(23, "W");
+        map.put(24, "X");
+        map.put(25, "Y");
+        map.put(26, "Z");
     }
 }
